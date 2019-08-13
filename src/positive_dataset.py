b8cb2fc6553ebecd2b247b71852160546db63ba7
LuisFranciscoHS <Luis.Sanchez@uib.no>
2019-08-13 17:55:49 +0200

    Biogrod download and conversion draft

--- src/positive_dataset.py
+++ src/positive_dataset.py
@@ -1,4 +1,15 @@
 #%%
+# Read configuration variables
+
+path_config = "config.txt"
+variables = {}
+file_config = open(path_config, "r")
+for line in file_config:
+    (key, value) = line.split()
+    variables[key] = value
+print(variables)
+
+#%%
 # Download the protein list from SwissProt
 import os
 path_swissprot_proteins = "data/UniProt/swissprot_human_proteins.tab"
@@ -26,9 +37,59 @@ if not os.path.exists(path_pathwaymatcher):
         r = requests.get(url_pathwaymatcher)
         os.makedirs(os.path.dirname(path_pathwaymatcher), exist_ok=True)
         open(path_pathwaymatcher, 'wb').write(r.content)
-        
+
 print("PathwayMatcher READY")
 
+import subprocess
+subprocess.run("java -jar tools/PathwayMatcher.jar match-uniprot -i data/UniProt/swissprot_human_proteins.tab -o data/Reactome/ -gu",
+    capture_output=False)
 
+print("Reactome interactions READY")
+
+#%%
+
+# Collect features of each Reactome interaction
+
+# Biogrid: if there is a reported humap PPI
+
+## Download Biogrid human gene interactions
+
+import os
+path_biogrid_gene_interactions = "data/Biogrid/human_gene_interactions.tab"
+
+if not os.path.exists(path_biogrid_gene_interactions):
+    print("Downloading Biogrid human gene interactions...")
+    os.makedirs(os.path.dirname(path_biogrid_gene_interactions), exist_ok=True)
+    import requests
+    request_result = requests.get("https://www.uniprot.org/uniprot/?query=reviewed:yes+AND+organism:9606&columns=id&format=tab")
+
+    with open(path_swissprot_proteins, "w") as file_swissprot_proteins:
+        file_swissprot_proteins.write(request_result.text)
+
+print("Biogrid gene interactions READY")
+
+#%%
+
+## Convert Biogrid interactions to protein interactions
+import urllib.parse
+import urllib.request
+
+url = 'https://www.uniprot.org/uploadlists/'
+
+params = {
+    'from': 'P_ENTREZGENEID',
+    'to': 'ACC',
+    'format': 'tab',
+    'query': '2318\n84665\n88\n90\n339\n6416',
+    'reviewed': 'yes'
+}
+
+data = urllib.parse.urlencode(params)
+data = data.encode('utf-8')
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as f:
+   response = f.read()
+print(response.decode('utf-8'))
+print("Biogrid protein interactions READY")
 
 #%%

437b0aaf8e2124d0fc394af8b1c367f0e6af44d8
LuisFranciscoHS <Luis.Sanchez@uib.no>
2019-08-23 19:12:29 +0200

    Merge datasets

--- src/dataset/dataset_loader.py
+++ src/dataset/dataset_loader.py
@@ -1,12 +1,31 @@
 import numpy as np
 import sklearn
 
+from config import read_config
 from dataset import reactome
 
-def merge_n_columns():
-    pass #TODO Implement merge_n_columns function
 
-def load_dataset(config):
+def merge_features(*features_arrays):
+    """Creates one np.array with one column for each feature"""
+    # np.concatenate((a, b.T), axis=1)
+    return features_arrays[0]
+
+
+def merge_datasets(*datasets):
+    """Concatenates the target vector, the features array and the interactions list"""
+    if len(datasets) == 0:
+        raise ValueError('Missing datasets to merge')
+
+    for dataset in datasets[1:]:
+        datasets[0]['features'] = np.concatenate((datasets[0]['features'], dataset['features']), axis=0)
+        datasets[0]['targets'] = np.concatenate((datasets[0]['targets'], dataset['targets']), axis=0)
+        datasets[0]['interactions'] = datasets[0]['interactions'] +  dataset['interactions']
+    return datasets[0]
+
+
+def load_dataset(path_config):
+
+    config = read_config(path_config)
 
     # Get Reactome dataset
     dataset_reactome = reactome.load_dataset(config)
@@ -14,9 +33,10 @@ def load_dataset(config):
     # Get String dataset TODO
 
     # Get negative dataset TODO
-    
+    dataset_negative = {}
 
     # Bind datasets together
+    merge_datasets(dataset_reactome, dataset_negative)
 
     return sklearn.utils.Bunch(
         features=dataset_reactome['features'],
--- src/tests/dataset/test_dataset_loader.py
+++ src/tests/dataset/test_dataset_loader.py
@@ -1,13 +1,15 @@
 import numpy as np
 from unittest import TestCase
+
+import sklearn
 from sklearn.datasets.base import Bunch
-from dataset.dataset_loader import load_dataset
+from dataset.dataset_loader import load_dataset, merge_datasets
 
 
-class Test_dataset_loader(TestCase):
+class TestDatasetLoader(TestCase):
 
     def setUp(self):
-        self.result = load_dataset()
+        self.result = load_dataset('../../../')
 
     def test_load_dataset_returns_bunch(self):
         self.assertTrue(type(self.result) == Bunch)
@@ -35,3 +37,41 @@ class Test_dataset_loader(TestCase):
                          msg="There should be a pair if interacting proteins for each sample in features.")
         self.assertEqual(self.result['features'].shape[0], self.result['target'].shape[0],
                          msg="There should be a target value for each sample in the features.")
+
+
+class TestMergeDatasets(TestCase):
+
+    def setUp(self):
+        self.dataset1 = sklearn.utils.Bunch(features=np.array([1, 0, 1, 0, 1]),
+                                            targets=np.array([True, False, True, False, True]),
+                                            interactions=['A', 'B', 'C', 'D', 'E'])
+
+        self.dataset2 = sklearn.utils.Bunch(features=np.array([1, 0, 1]),
+                                            targets=np.array([True, False, True]),
+                                            interactions=['F', 'G', 'H'])
+
+    def test_merge_datasets_no_dataset_raises_error(self):
+        with self.assertRaises(ValueError):
+            merge_datasets()
+
+    def test_merge_datasets_one_dataset(self):
+        self.dataset1 = merge_datasets(self.dataset1)
+        self.assertEqual((5,), self.dataset1['features'].shape, msg='The length of the features vector should not change.')
+        self.assertEqual((5,), self.dataset1['targets'].shape, msg='The length of the targets vector should not change.')
+        self.assertEquals(5, len(self.dataset1['interactions']), msg='The number of interactions should not change')
+
+    def test_merge_datasets_merges_multiple(self):
+        dataset3 = merge_datasets(self.dataset1, self.dataset2)
+
+        self.assertEqual((8,), dataset3['features'].shape, msg='Wrong length of features vector')
+        self.assertEqual((8,), dataset3['targets'].shape, msg='Wrong length of target vector')
+        self.assertEquals(8, len(dataset3['interactions']), msg='Wrong length of interactions list')
+
+
+class TestMergeFeatures(TestCase):
+
+    def test_merge_features_one_feature(self):
+        self.fail()
+
+    def test_merge_features_multiple(self):
+        self.fail()

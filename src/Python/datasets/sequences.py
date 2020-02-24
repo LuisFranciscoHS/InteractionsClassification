28c1944805865e94a2bb16fdb8e450f5e45d598a
Luis Francisco Hernández Sánchez <franciscohsmex@gmail.com>
2020-02-24 23:51:42 +0100

    Create empty file structure for models

---
+++ src/Python/datasets/sequences.py
@@ -0,0 +1,6 @@
+
+def load_data():
+    """Create dataset of candidate sequences to become Pathways
+
+    """
+    pass
---
+++ src/Python/datasets/toronto.py
@@ -0,0 +1,21 @@
+import numpy as np
+import csv
+
+
+def load_data():
+    """PPI Dataset replica of study from Ontario Institute for Cancer Research
+
+    :returns 2 tuples
+        * X_train, X_test:
+        * y_train, y_test:
+
+    """
+
+    # Gather interactions from Reactome
+
+    # For each of them get table saying
+    X = np.zeroes()
+    return ((), ())
+
+def save():
+    pass
